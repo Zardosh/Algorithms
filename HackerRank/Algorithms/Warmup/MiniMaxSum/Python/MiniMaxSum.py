8e3279497c89b2bc252971700b7d2fc4c17450e9
Zardosh <54995376+Zardosh@users.noreply.github.com>
2019-12-09 14:05:53 -0300

    Create MiniMaxSum.py

---
+++ HackerRank/Algorithms/Warmup/MiniMaxSum/Python/MiniMaxSum.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the miniMaxSum function below.
+def miniMaxSum(arr):
+    arr.sort()
+
+    print(sum(arr[:-1:]), sum(arr[1::]))
+
+# Challenge definitions
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
