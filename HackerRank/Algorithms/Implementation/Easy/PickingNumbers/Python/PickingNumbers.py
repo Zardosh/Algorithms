bd62bad01b07a0d5ef1ca90faee04a9214e06843
Eduardo Zardo <eduzarrdo@gmail.com>
2019-12-11 18:13:18 -0300

    Create PickingNumbers.py

---
+++ HackerRank/Algorithms/Implementation/Easy/PickingNumbers/Python/PickingNumbers.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    # Write your code here
+    # Returns the max value on a list comprehension of the lengths of all possible
+    # subarrays with consecutive numbers
+    return max([a.count(number) + a.count(number + 1) for number in set(a)])
+
+# Challenge definitions
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
